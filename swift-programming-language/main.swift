d8306b0d56dfb76a10639a4e7ee7e3c0fdc0f6bc
Armağan Gök <1armagangok@gmail.com>
2021-11-21 02:22:37 +0300

    first commit

--- swift-programming-language/main.swift
+++ swift-programming-language/main.swift
@@ -7,5 +7,43 @@
 
 import Foundation
 
+
+
 print("Hello, World!")
+
+
+
+// In Swift, we use the var keyword to declare variables. For example,
+var num = 10
+
+
+// siteName is a variable of type String. Meaning, it can only store textual values.
+// Here, siteName is a variable of type String. Meaning, it can only store textual values.
+// id is a variable of Int type. Meaning, it can only store integer values.
+var siteName:String
+var id:Int
+
+// NOTE! => In Swift, we cannot change the type of a variable once it's declared.
+
+
+siteName = "apple"
+
+// Variables names must start with either a letter, an underscore _, or the dollar sign $. For example,
+var a = "hello"
+var _a = "hello"
+
+// Variable names cannot start with numbers. For example,
+// Swift is case-sensitive. So A and a are different variables. For example,
+
+// Avoid using Swift keywords like var, String, class, etc. as variable names.
+
+// It's a good practice to give a descriptive variable name. For example, numberofApples is a better variable name than a, apple, or n.
+// In Swift, variable names are generally written in camelCase if they have multiple words. For example, myVariable, addTwoNums, etc.It's a good practice to give a descriptive variable name. For example, numberofApples is a better variable name than a, apple, or n.
+// Swift, variable names are generally written in camelCase if they have multiple words. For example, myVariable, addTwoNums, etc.
+
+
+
+
+
+
 
